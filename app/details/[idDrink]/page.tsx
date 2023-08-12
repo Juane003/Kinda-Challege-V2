c6a541a40c603232cfbb3ad6fc741239cb0e6570
Juane003 <j.emilio164@gmail.com>
2023-08-12 13:17:17 -0300

    removed generateStaticParams

--- app/details/[idDrink]/page.tsx
+++ app/details/[idDrink]/page.tsx
@@ -1,13 +1,13 @@
 import { CocktailDescription } from "@/components/CocktailDescription";
 import { CocktailDetailResponse, CocktailsResponse } from "@/types";
 
-export async function generateStaticParams() {
-  const data = await fetch(process.env.API_LIST!);
-  const cocktails = (await data.json()) as CocktailsResponse;
-  return cocktails.drinks.map((cocktail) => {
-    return { idDrink: cocktail.idDrink };
-  });
-}
+// export async function generateStaticParams() {
+//   const data = await fetch(process.env.API_LIST!);
+//   const cocktails = (await data.json()) as CocktailsResponse;
+//   return cocktails.drinks.map((cocktail) => {
+//     return { idDrink: cocktail.idDrink };
+//   });
+// }
 
 const getCurrentDrink = async (id: string) => {
   const data = await fetch(process.env.API_DRINK_BY_ID! + id);
